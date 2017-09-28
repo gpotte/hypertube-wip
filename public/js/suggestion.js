4c459b793725900e2c56a20e9543d4ed019eb943
Gilles POTTE <gpotte@e2r5p2.42.fr>
2017-09-28 17:11:32 +0200

    HD button added

--- public/js/suggestion.js
+++ public/js/suggestion.js
@@ -1,7 +1,10 @@
 var page = 1;
 function getMovies(){
   $.getJSON('https://yts.ag/api/v2/list_movies.json?sort_by=like_count&with_rt_ratings=true&limit=24&page='+page, (data)=>{
+    data.data.movies.sort((a, b)=>{return (b.torrents[0].seeds - a.torrents[0].seeds)});
     data.data.movies.forEach((movie)=>{
+      console.log(movie);
+      if (movie.torrents[1]){
       $('.container').append('<div class="row-eq-height col-xs-5 col-md-3 thumbnail">\
                                   <img src="'+movie.large_cover_image+'">\
                                   <div class="caption">\
@@ -9,8 +12,20 @@ function getMovies(){
                                     <small>'+movie.summary.substring(0, 100)+'</small><br>\
                                     <button class="btn btn-default">'+movie.rating+'/10</button>\
                                     <a href="http://localhost:3030/movie/'+encodeURI(movie.title)+'/'+movie.torrents[0].hash+'" class="btn btn-success">Watch</a>\
+                                    <a href="http://localhost:3030/movie/'+encodeURI(movie.title)+'/'+movie.torrents[1].hash+'" class="btn btn-success">Watch HD</a>\
                                   </div>\
                                 </div>')
+      } else {
+        $('.container').append('<div class="row-eq-height col-xs-5 col-md-3 thumbnail">\
+                                    <img src="'+movie.large_cover_image+'">\
+                                    <div class="caption">\
+                                      <h3>'+movie.title+'</h3>\
+                                      <small>'+movie.summary.substring(0, 100)+'</small><br>\
+                                      <button class="btn btn-default">'+movie.rating+'/10</button>\
+                                      <a href="http://localhost:3030/movie/'+encodeURI(movie.title)+'/'+movie.torrents[0].hash+'" class="btn btn-success">Watch</a>\
+                                    </div>\
+                                  </div>')
+      }
     });
   });
   page++;
--- routes/video.js
+++ routes/video.js
@@ -1,7 +1,16 @@
 router.get('/movie/:title/:hash', middleware.loggedIn(), (req, res)=>{
   var room = req.params.title + encodeURI(Math.trunc(Math.random() * 10000000)),
-  magnet = 'magnet:?xt=urn:btih:'+req.params.hash+'&dn='+ encodeURI(req.params.title)+'&tr=udp://glotorrents.pw:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://torrent.gresille.org:80/announce&tr=udp://tracker.openbittorrent.com:80&tr=udp://tracker.coppersurfer.tk:6969&tr=udp://tracker.leechers-paradise.org:6969&tr=udp://p4p.arenabg.ch:1337&tr=udp://tracker.internetwarriors.net:1337';
-  var engine = torrentStream(magnet, {path: '/tmp/test'});
+  magnet = 'magnet:?xt=urn:btih:'+req.params.hash+'&dn='+ encodeURI(req.params.title);
+  var engine = torrentStream(magnet, {path: '/tmp/test', trackers: ["udp://glotorrents.pw:6969/announce",
+                                      "udp://tracker.opentrackr.org:1337/announce",
+                                      "udp://torrent.gresille.org:80/announce",
+                                      "udp://tracker.openbittorrent.com:80",
+                                      "udp://tracker.coppersurfer.tk:6969",
+                                      "udp://tracker.leechers-paradise.org:6969",
+                                      "udp://p4p.arenabg.ch:1337",
+                                      "udp://tracker.internetwarriors.net:1337"
+  ]});
+
   engine.on('ready', ()=>{
     const max = engine.files.reduce((prev, current)=>{
       return (prev.length > current.length) ? prev : current;
@@ -10,8 +19,8 @@ router.get('/movie/:title/:hash', middleware.loggedIn(), (req, res)=>{
       if (file !== max)
       {
         file.deselect();
-        fs.unlink('/tmp/test/'+file.path, ()=>{
-          console.log('removing '.red, file.path)
+          fs.unlink('/tmp/test/'+file.path, ()=>{
+            console.log('removing '.red, file.path)
         });
       }
       else {
@@ -26,108 +35,33 @@ router.get('/movie/:title/:hash', middleware.loggedIn(), (req, res)=>{
 router.get('/video', (req, res)=>{
   let file = '/tmp/test/' + decodeURI(req.query.path);
   fs.stat(file, function(err, stats) {
-
-  		//
-  		//	1.	If there was an error reading the file stats we inform the
-  		//		browser of what actual happened
-  		//
   		if(err)
   		{
-  			//
-  			//	1.	Check if the file exists
-  			//
   			if(err.code === 'ENOENT')
-  			{
-  				//
-  				// 	->	404 Error if file not found
-  				//
+        {
   				return res.sendStatus(404);
   			}
-
-  			//
-  			//	2.	IN any other case, just output the full error
-  			//
-  			return next(err)
+        return next(err)
   		}
-
-  		//
-  		//	2.	Save the range the browser is asking for in a clear and
-  		//		reusable variable
-  		//
-  		//		The range tells us what part of the file the browser wants
-  		//		in bytes.
-  		//
-  		//		EXAMPLE: bytes=65534-33357823
-  		//
   		let range = req.headers.range;
-
-  		//
-  		//	3.	Make sure the browser ask for a range to be sent.
-  		//
   		if(!range)
   		{
-  			//
-  			// 	1.	Create the error
-  			//
   			let err = new Error('Wrong range');
   				err.status = 416;
-
-  			//
-  			//	->	Send the error and stop the request.
-  			//
   			return next(err);
   		}
-
-  		//
-  		//	4.	Convert the string range in to an array for easy use.
-  		//
   		let positions = range.replace(/bytes=/, '').split('-');
-
-  		//
-  		//	5.	Convert the start value in to an integer
-  		//
   		let start = parseInt(positions[0], 10);
-
-  		//
-  		//	6.	Save the total file size in to a clear variable
-  		//
   		let file_size = stats.size;
-
-  		//
-  		//	7.	IF 		the end parameter is present we convert it in to an
-  		//				integer, the same way we did the start position
-  		//
-  		//		ELSE 	We use the file_size variable as the last part to be
-  		//				sent.
-  		//
   		let end = positions[1] ? parseInt(positions[1], 10) : file_size - 1;
-
-  		//
-  		//	8.	Calculate the amount of bits will be sent back to the
-  		//		browser.
-  		//
   		let chunksize = (end - start) + 1;
-
-  		//
-  		//	9.	Create the header for the video tag so it knows what is
-  		//		receiving.
-  		//
   		let head = {
   			'Content-Range': 'bytes ' + start + '-' + end + '/' + file_size,
   			'Accept-Ranges': 'bytes',
   			'Content-Length': chunksize,
   			'Content-Type': 'video/mp4'
   		}
-
-  		//
-  		//	10.	Send the custom header
-  		//
   		res.writeHead(206, head);
-
-  		//
-  		//	11.	Create the createReadStream option object so createReadStream
-  		//		knows how much data it should be read from the file.
-  		//
       if (start > end)
       {
         start = 0;
@@ -137,32 +71,13 @@ router.get('/video', (req, res)=>{
   			start: start,
   			end: end
   		}
-
-  		//
-  		//	12.	Create a stream chunk based on what the browser asked us for
-  		//
   		let stream = fs.createReadStream(file, stream_position)
-
-  		//
-  		//	13.	Once the stream is open, we pipe the data through the response
-  		//		object.
-  		//
   		stream.on('open', function() {
-
   			stream.pipe(res);
-
   		})
-
-  		//
-  		//	->	If there was an error while opening a stream we stop the
-  		//		request and display it.
-  		//
   		stream.on('error', function(err) {
-
   			return next(err);
-
   		});
-
   	});
 });
 
