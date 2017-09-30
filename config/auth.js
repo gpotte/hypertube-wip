acb689776b94d5a8257be86dfd13ecc4cbe25bfa
scredii <abourgeu@student.42.fr>
2017-09-30 14:09:31 +0200

    Connexion via Github added, need to check if email and firstname are ok

--- config/auth.js
+++ config/auth.js
@@ -8,5 +8,10 @@ module.exports = {
     'clientID'       :  '335ebb2d700bac12c778abeeacded65e7ec30e7e68ab1cbec8068404b5be61c3',
     'clientSecret'   :  'de5a06eda2d636dc25b1e023b38c8fef17d13a4de9813353e2ff9e70a26d5e64',
     'callbackURL'    :  'http://localhost:3030/auth/intra/callback'
+  },
+    'githubAuth'        :  {
+    'clientID'       :  'b421894d092b1026a70d',
+    'clientSecret'   :  'd357fc3413c3492a4366ab5634c2225b4a0aef31',
+    'callbackURL'    :  'http://localhost:3030/auth/github/callback'
   }
 };
