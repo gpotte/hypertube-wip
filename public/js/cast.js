bd3ae408d4af374c060fd53ab9a3cd21de68136d
Alexandre BOURGEUS <abourgeu@e2r8p8.42.fr>
2017-10-02 15:21:50 +0200

    remove console log

--- public/js/cast.js
+++ public/js/cast.js
@@ -1,4 +1,3 @@
 $.getJSON('http://www.theimdbapi.org/api/movie?movie_id='+ imdb_code, (data)=>{
-    console.log(data.stars);
    $('#description').append('<h5> Casting: '+ data.stars +'<h5>')
   });
